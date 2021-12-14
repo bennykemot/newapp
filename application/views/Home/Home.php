221edd4c2891debc593e0ecf0f7d6c6a83151382
Nur Haryani <nurharyani2407@gmail.com>
2021-12-14 12:49:14 +0700

    dashboard

--- application/views/Home/Home.php
+++ application/views/Home/Home.php
@@ -1,6 +1,6 @@
 <?php include(APPPATH . 'views/Header/Aside.php') ?>
 
-  <!--Blog Card-->
+  <!-- Blog Card
   <div id="card-panel-type" class="section">
     <div class="row mt-2">
       <div class="col s12 m6 l12 card-width">
@@ -8,20 +8,113 @@
           <img class="responsive-img border-radius-8 z-depth-4 image-n-margin" src="<?= base_url().'assets'?>/app-assets/images/logo/LOGO-BISMA.png" alt="images">
 
 					<div class="col s12 m6 l6 card-width">
-			<h3><a href="#" class="mt-5">DASHBOARD</a></h3>
-          <h6>Sedang dalam pengembangan</h6>
-					<h6>Sabar ya</h6>
+						<h3><a href="#" class="mt-5">DASHBOARD</a></h3>
+								<h6>Sedang dalam pengembangan</h6>
+								<h6>Sabar ya</h6>
 
-			</div>
+					</div>
         </div>
-				
-				
-      </div>
-
-      
-      
+      </div>      
     </div>
-  </div>
+  </div> -->
+
+
+	   <!--card stats start-->
+		 <div id="card-stats" class="pt-0">
+      <div class="row">
+         <div class="col s12 m6 l6 xl3">
+            <div class="card gradient-45deg-light-blue-cyan gradient-shadow min-height-100 white-text animate fadeLeft">
+               <div class="padding-4">
+                  <div class="row">
+                     <div class="col s4 m2">
+                        <i class="material-icons background-round mt-5">timeline</i>
+                        <p></p>
+                     </div>
+                     <div class="col s8 m10 center-align">
+                        <h5 class="mb-0 white-text">Dashboard</h5>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="col s12 m6 l6 xl3">
+            <div class="card gradient-45deg-red-pink gradient-shadow min-height-100 white-text animate fadeLeft">
+               <div class="padding-4">
+                  <div class="row">
+                     <div class="col s4 m2">
+                        <i class="material-icons background-round mt-5">attach_money</i>
+                        <p></p>
+                     </div>
+                     <div class="col s8 m10 center-align">
+                        <h5 class="mb-0 white-text">Anggaran</h5>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="col s12 m6 l6 xl3">
+            <div class="card gradient-45deg-amber-amber gradient-shadow min-height-100 white-text animate fadeRight">
+               <div class="padding-4">
+                  <div class="row">
+                     <div class="col s4 m2">
+                        <i class="material-icons background-round mt-5">domain</i>
+                        <p></p>
+                     </div>
+                     <div class="col s8 m10 center-align">
+                        <h5 class="mb-0 white-text">Rencana Kegiatan</h5>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="col s12 m6 l6 xl3">
+            <div class="card gradient-45deg-green-teal gradient-shadow min-height-100 white-text animate fadeRight">
+               <div class="padding-4">
+                  <div class="row">
+                     <div class="col s4 m2">
+                        <i class="material-icons background-round mt-5">people</i>
+                        <p></p>
+                     </div>
+                     <div class="col s8 m10 center-align">
+                        <h5 class="mb-0 white-text">Pengaturan</h5>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+   <!--card stats end-->
+
+	    <!--yearly & weekly revenue chart start-->
+			<div id="sales-chart">
+      <div class="row">
+         <div class="col s12 m8 l8">
+            <div id="revenue-chart" class="card animate fadeUp">
+               <div class="card-content center-align">
+									
+										<h4 class="m-0"><b>Dashboard</b></h4>
+										<h5>Sedang dalam pengembangan</h5>
+										<h6 class="amber-text mt-2">
+											Mohon bersabar ya
+											</h6>
+											<i class="material-icons amber-text small-ico-bg mb-5">tag_faces</i>
+               </div>
+            </div>
+         </div>
+         <div class="col s12 m4 l4">
+            <div id="weekly-earning" class="card animate fadeUp">
+               <div class="card-content">
+							 		<img class="responsive-img border-radius-8 z-depth-4" src="<?= base_url().'assets'?>/app-assets/images/logo/LOGO-BISMA.png" alt="images">
+               </div>
+            </div>
+         </div>
+
+
+
+</div>
+</div>
+   <!--yearly & weekly revenue chart end-->
           
 				
 <?php include(APPPATH . 'views/Footer/Footer.php') ?>
