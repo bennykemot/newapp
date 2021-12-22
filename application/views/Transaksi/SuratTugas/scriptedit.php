9079e23b7ca273af8fdff85a4f1bb835b6c7eda7
Nur Haryani <nurharyani2407@gmail.com>
2021-12-22 15:17:42 +0700

    ttd for ST

--- application/views/Transaksi/SuratTugas/scriptedit.php
+++ application/views/Transaksi/SuratTugas/scriptedit.php
@@ -502,6 +502,8 @@ $("#user-select2").select2({
            delay: 250,
            data: function (params) {
               return {
+								Trigger: "ttd_forST",
+								kdsatker: satker_session,
                 searchTerm: params.term // search term
               };
            },
